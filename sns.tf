f3c9abfb2d5c01e7223091280e0a65f655d18abe
Richard Annand <richard+git@ohheyrj.co.uk>
2022-09-23 23:09:36 +0100

    Revert "fix: added sns topic encryption"
    This reverts commit 35f486a592e3b0c408b78c9e09a397b3749b023a.

--- sns.tf
+++ sns.tf
@@ -1,10 +1,9 @@
 resource "aws_sns_topic" "cis_sns_topic" {
-  name              = "CIS-SNS-Topic"
-  kms_master_key_id = "alias/aws/sns"
+  name = "CIS-SNS-Topic"
 }
 
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.cis_sns_topic.arn
   protocol  = "email"
-  endpoint  = var.sns_email
+  endpoint = var.sns_email
 }
