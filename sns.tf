35f486a592e3b0c408b78c9e09a397b3749b023a
Richard Annand <richard+git@ohheyrj.co.uk>
2022-09-23 19:23:48 +0100

    fix: added sns topic encryption

--- sns.tf
+++ sns.tf
@@ -1,9 +1,10 @@
 resource "aws_sns_topic" "cis_sns_topic" {
-  name = "CIS-SNS-Topic"
+  name              = "CIS-SNS-Topic"
+  kms_master_key_id = "alias/aws/sns"
 }
 
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.cis_sns_topic.arn
   protocol  = "email"
-  endpoint = var.sns_email
+  endpoint  = var.sns_email
 }
