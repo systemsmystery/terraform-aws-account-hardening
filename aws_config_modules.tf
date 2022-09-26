87c499ffff72524a4dca8727d7d0514ef0f0acd0
Richard Annand <richard+git@ohheyrj.co.uk>
2022-09-27 00:05:02 +0100

    style: added module from registry instead of directly from git

--- aws_config_modules.tf
+++ aws_config_modules.tf
@@ -1,40 +1,52 @@
+module "config-enable-module" {
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
+  # insert the 2 required variables here
+}
+
 module "config-ap-northeast-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-northeast-1"
 }
 
 module "config-ap-northeast-2" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-northeast-2"
 }
 
 module "config-ap-northeast-3" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-northeast-3"
 }
 
 module "config-ap-south-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-south-1"
 }
 
 module "config-ap-southeast-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-southeast-1"
 }
 
 module "config-ap-southeast-2" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ap-southeast-2"
@@ -48,21 +60,24 @@ module "config-ap-southeast-2" {
 # }
 
 module "config-ca-central-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "ca-central-1"
 }
 
 module "config-eu-central-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "eu-central-1"
 }
 
 module "config-eu-north-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "eu-north-1"
@@ -76,56 +91,64 @@ module "config-eu-north-1" {
 # }
 
 module "config-eu-west-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "eu-west-1"
 }
 
 module "config-eu-west-2" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "eu-west-2"
 }
 
 module "config-eu-west-3" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "eu-west-3"
 }
 
 module "config-sa-east-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "sa-east-1"
 }
 
 module "config-us-east-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "us-east-1"
 }
 
 module "config-us-east-2" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "us-east-2"
 }
 
 module "config-us-west-1" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "us-west-1"
 }
 
 module "config-us-west-2" {
-  source           = "git@github.com:systemsmystery/terraform-aws-config-enable-module.git"
+  source  = "systemsmystery/config-enable-module/aws"
+  version = "1.0.0"
   iam_role_arn     = aws_iam_role.config.arn
   config_bucket_id = aws_s3_bucket.config_bucket.id
   region           = "us-west-2"
